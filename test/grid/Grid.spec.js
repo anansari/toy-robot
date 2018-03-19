f77a2637e3748abe386db87d14ed6d005dfb3ad7
anansari <aneel.ansari@ymail.com>
2018-03-19 21:49:48 +0800

    Fixes the bug. It somehow didn't work when Grid and Position objects were created above it

--- test/grid/Grid.spec.js
+++ test/grid/Grid.spec.js
@@ -9,8 +9,6 @@ describe("Checks if the position being set is valid", () => {
     });
 
     it("should be a valid postion if it's under grid size 5x5", () => {
-        let position = new Position(1, 11, "WEST");
-        let grid = new Grid();
-        expect(Grid.isPositionValid((new Position(11, 0, "WEST")))).to.be.true;
+        expect(Grid.isPositionValid((new Position(11, 0, "WEST")))).to.be.false;
     });
 });
